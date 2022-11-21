6173bc5b4ae8aa7b95bab100c0f378f09e3edf7e
x4NT0Nx <115920674+x4NT0Nx@users.noreply.github.com>
2022-11-21 12:43:43 +0300

    Added files and changes required for deployment to heroku

--- rekruto/settings.py
+++ rekruto/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
